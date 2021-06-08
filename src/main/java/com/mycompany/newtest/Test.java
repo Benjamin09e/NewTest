40483efebbd90becd411669e350262a3938fded6
Admin <ekiabenjamin@gmail.com>
2021-06-09 01:43:49 +0200

    commit

--- src/main/java/com/mycompany/newtest/Test.java
+++ src/main/java/com/mycompany/newtest/Test.java
@@ -1,7 +1,7 @@
 package com.mycompany.newtest;
 
 public class Test {
-    public static void main(String... args){
+    public static void main(  String... args) {
         System.out.println("Hello World");
     }
 }
